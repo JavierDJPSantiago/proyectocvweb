b8cff6619145ceae4d0743ac91331205e55c58a0
Javier de Jesús Pérez Santiago <javier.de.jesus.perez.santiago@gmail.com>
2022-01-05 17:51:45 -0600

    first commit

---
+++ web.js
@@ -0,0 +1,6 @@
+var resultado = window.confirm("¿Estas seguro de entrar a mi CV web?");
+if(resultado===true){
+    window.alert("Ok, gracias por entrar");
+} else {
+    window.alert("Espero que decidas volver, lindo día")
+}
